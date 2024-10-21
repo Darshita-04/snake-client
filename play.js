4735ac93e2fa8cd255a8361b74d0badb750addaf
Darshita Patel <pateldarshita1994@gmail.com>
2024-10-21 10:47:58 -0400

    Connect to the Server

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.255.255.254', // change to IP address of computer, more on that below
+    port: 50541,
+  });
+
+  conn.on("connect", () => {
+    console.log("Connected to the server!");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Received from server: ", data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
