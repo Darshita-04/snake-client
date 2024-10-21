cdc93238bc2196340a0d8bf67f2c5915b026e803
Darshita Patel <pateldarshita1994@gmail.com>
2024-10-21 10:54:40 -0400

    Connection Module

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost', // change to IP address of computer, more on that below
+    port: 50541,
+  });
+
+  conn.on("connect", () => {
+    console.log("Connected to the server!");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Received from server: ", data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+console.log("Connecting ...");
+connect();
+
+
+module.exports = connect;
